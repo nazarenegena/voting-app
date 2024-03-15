4099f703100cdcfa15c4da481c5c02142ba1f4f5
Nazarene Gena <genarene96@gmail.com>
2024-03-15 17:27:51 +0300

    set up the initial routes

---
+++ src/authentication/LogOut.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const LogOut = () => {
+  return (
+    <div>
+      LogOut section
+      <Link to="/">Logout</Link>
+    </div>
+  );
+};
+
+export default LogOut;
---
+++ src/authentication/Register.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+const Register = () => {
+  return (
+    <div>
+      <p>Register section</p>
+      <Link to="/login">Click toRegister</Link>
+    </div>
+  );
+};
+
+export default Register;
